d84aa03498646b8e9bb90415c468ef373e330f39
carlosaquino <eduarduo.aquino@gmail.com>
2026-09-11 17:00:56 -0300

    feat: admin movimentacao criado

--- movimentacao/admin.py
+++ movimentacao/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from movimentacao import models
 
-# Register your models here.
+@admin.register(models.Movimentacao)
+class MovimentacaoAdmin(admin.ModelAdmin):
+    list_display=('equipamento','codigo','origem','destino','uso','observacao','usuario','data_criacao','status')
+    ordering=('-data_criacao',)
+    list_per_page=10
