ff08246c01d8f661c8ebae8c03440162e4df4389
carlosaquino <eduarduo.aquino@gmail.com>
2026-09-15 14:41:36 -0300

    feat: add mudança de status para administradores

--- movimentacao/urls.py
+++ movimentacao/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('nova/', views.criar_movimentacao, name= 'criar_movimentacao'),
     path('<int:id>', views.detalhe_movimentacao, name='detalhe_movimentacao'),
     path('<int:id>/editar/', views.editar_movimentacao, name='editar_movimentacao'),
-    path('<int:id>/exluir/', views.excluir_movimentacao, name='excluir_movimentacao')
+    path('<int:id>/exluir/', views.excluir_movimentacao, name='excluir_movimentacao'),
+    path('<int:id>/status/', views.alterar_status, name='alterar_status'),
+
 ]
--- movimentacao/views.py
+++ movimentacao/views.py
@@ -102,3 +102,24 @@ def excluir_movimentacao(request, id):
     movimentacao.delete()
 
     return redirect('lista_movimentacoes')
+
+
+@login_required
+@administrador_required
+@require_POST
+def alterar_status(request, id):
+    movimentacao = get_object_or_404(
+        Movimentacao,
+        id=id
+    )
+
+    novo_status = request.POST.get('status')
+
+    if novo_status in ['pendente', 'processado', 'descartado']:
+        movimentacao.status = novo_status
+        movimentacao.save(update_fields=['status'])
+
+    return redirect(
+        'detalhe_movimentacao',
+        id=movimentacao.id
+    )
